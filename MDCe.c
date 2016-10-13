5d173b85e6dcdd08d0e80f3a19dfda082833d59b
Rblacknight <riccardoideaemail@gmail.com>
2016-10-13 15:11:12 +0200

    Create MDCe.c

---
+++ MDCe.c
@@ -0,0 +1,47 @@
+/*  Programma per verificare la velocita di calcolo tra MCD con metodo
+*   tradizionale e con l' algoritmo di Euclide
+*/
+
+#include<stdio.h>
+
+int Euc(int x, int y);
+
+int main(){
+  /* Dichiarazione Variabili */
+  int = a;
+  int = b;
+  
+  /* Introduzione */
+  printf("Inserire a");
+  scanf( "%d",&a );
+  printf("Inserire b");
+  scanf( "%d",&b );
+  
+  /* Svolgimento */
+  printf("MCD tra a =%d e b =%d è%d", a, b, Euc(a,b));
+  
+  return 0;
+
+}
+
+/*----------------------------------------------------------------------------*/
+
+int Euc (int x, int y){
+  /* Dichiarazione Variabili */
+  int x;
+  int y;
+  
+  /* Svolgimento */
+  x = a;
+  y = b;
+  
+   while (y != 0){
+  /*1  x = y;
+  
+  *  y = x % y;
+  */  
+  }
+   
+   return x;
+   
+}
